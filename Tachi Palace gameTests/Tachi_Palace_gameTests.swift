6bf09f6d306eb0a826affdde5ee90bcf629b3372
komiselena <komiselen@yandex.ru>
2025-05-20 12:35:31 +0300

    Initial Commit

---
+++ Tachi Palace gameTests/Tachi_Palace_gameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tachi_Palace_gameTests.swift
+//  Tachi Palace gameTests
+//
+//  Created by Mac on 20.05.2025.
+//
+
+import Testing
+@testable import Tachi_Palace_game
+
+struct Tachi_Palace_gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
